332b59e56d1da6139a1bd64adbad23df89372f66
Nicolas Rakover <nrakover@mit.edu>
2018-02-27 20:21:23 -0800

    eval training set performance

--- rl_trader/runner.py
+++ rl_trader/runner.py
@@ -4,6 +4,7 @@ Scripts for training and evaluating RL agents.
 
 import random
 
+import numpy as np
 import tensorflow as tf
 
 from .coin_rl.agent import StochasticAgent
@@ -12,11 +13,17 @@ from .coin_rl.policy_gradient import PGNetwork
 
 from .utils.metrics import MetricsLogger
 
-def train(env_config, num_assets, num_iterations, network_layers, discount_gamma=0.999, target_buffer_size=32000, verbose=True, tracemalloc=None, num_malloc_stats=5):
+def train(env_config, num_assets, num_iterations, network_layers, discount_gamma=0.999, target_buffer_size=32000, eval_frequency=0, verbose=True, tracemalloc=None, num_malloc_stats=5):
     # init PG network
     sess = tf.Session()
     policy = PGNetwork(sess, num_assets, network_layers, discount_gamma)
     policy.initialize()
+
+    # establish performance baseline
+    if eval_frequency > 0:
+        print(">>>>> Pre-training baseline performance")
+        eval(env_config, num_assets, policy, detailed_report=verbose)
+        print("---------------------------------")
 
     # training iterations
     for iteration in range(num_iterations):
@@ -56,8 +63,7 @@ def train(env_config, num_assets, num_iterations, network_layers, discount_gamma
             num_episodes_run += 1
             buffer_size += len(current_path)
 
-            if verbose:
-                print("finished episode {}".format(num_episodes_run-1))
+            print("finished episode {}".format(num_episodes_run-1))
             if tracemalloc is not None:
                 snapshot = tracemalloc.take_snapshot()
                 stats = snapshot.statistics("lineno")
@@ -66,22 +72,73 @@ def train(env_config, num_assets, num_iterations, network_layers, discount_gamma
 
             # flush experience buffer and update policy parameters
             if buffer_size >= target_buffer_size:
-                _do_policy_update(policy, paths, verbose)
+                _do_policy_update(policy, paths)
                 paths.clear()
                 buffer_size = 0
 
         # update policy parameters
-        _do_policy_update(policy, paths, verbose)
+        _do_policy_update(policy, paths)
 
-        if verbose:
-            print("Finished Iteration {0}".format(iteration))
-            print("---------------------------------")
+        # report progres
+        print("Finished Iteration {0}".format(iteration))
+        if eval_frequency > 0 and iteration % eval_frequency == 0:
+            eval(env_config, num_assets, policy, detailed_report=verbose)
+        print("---------------------------------")
 
     return policy
 
-def _do_policy_update(policy, paths, verbose):
-    if verbose:
-        print(">>>>>>>> updating policy")
+def _do_policy_update(policy, paths):
+    print(">>>>> updating policy")
     if len(paths) == 0:
         return
     policy.take_gradient_step(paths)
+
+def eval(env_config, num_assets, policy, detailed_report=True):
+    # init environment
+    logger = MetricsLogger()
+    env = Env(env_config, metrics_logger=logger)
+
+    # init agent
+    agent = StochasticAgent(num_assets, policy)
+
+    rewards = []
+    while env.has_next_episode():
+        state = env.reset()
+        done = False
+        while not done:
+            # choose best action
+            action = agent.get_best_action(state)
+            # act
+            state, reward, done = env.step(action)
+            rewards.append(reward)
+    logger.finalize()
+
+    # report results
+    print("Number of steps: {}".format(len(rewards)))
+    print("Mean reward: {}".format(np.mean(rewards)))
+    if detailed_report:
+        _produce_detailed_report(logger.summaries)
+
+def _produce_detailed_report(summary_stats):
+    print("Number of episodes: {}".format(len(summary_stats)))
+
+    mean_total_growth = np.mean([x for (_, x, _, _) in summary_stats])
+    std_total_growth = np.std([x for (_, x, _, _) in summary_stats])
+    min_total_growth = np.min([x for (_, x, _, _) in summary_stats])
+    print("MEAN total growth: {}".format(mean_total_growth))
+    print("STD total growth: {}".format(std_total_growth))
+    print("MIN total growth: {}".format(min_total_growth))
+
+    mean_avg_daily_growth = np.mean([x for (_, _, x, _) in summary_stats])
+    std_avg_daily_growth = np.std([x for (_, _, x, _) in summary_stats])
+    min_avg_daily_growth = np.min([x for (_, _, x, _) in summary_stats])
+    print("MEAN avg daily growth: {}".format(mean_avg_daily_growth))
+    print("STD avg daily growth: {}".format(std_avg_daily_growth))
+    print("MIN avg daily growth: {}".format(min_avg_daily_growth))
+
+    mean_relative_growth = np.mean([x for (_, _, _, x) in summary_stats])
+    std_relative_growth = np.std([x for (_, _, _, x) in summary_stats])
+    min_relative_growth = np.min([x for (_, _, _, x) in summary_stats])
+    print("MEAN relative growth: {}".format(mean_relative_growth))
+    print("STD relative growth: {}".format(std_relative_growth))
+    print("MIN relative growth: {}".format(min_relative_growth))
--- rl_trader/utils/metrics.py
+++ rl_trader/utils/metrics.py
@@ -2,22 +2,25 @@
 Utils for evaluating episodes and experiments.
 '''
 
-import math
+import numpy as np
 
 SECOND_PER_DAY = 60*60*24
 
 def compute_total_growth(rewards):
-    total_reward = sum(rewards) # sum of logs of growth factors
-    return math.exp(total_reward)
+    total_reward = np.sum(rewards) # sum of logs of growth factors
+    return np.exp(total_reward)
 
-def compute_avg_growth_per_day(episode, rewards):
-    total_growth = compute_total_growth(rewards)
-
+def compute_episode_length_in_days(episode):
     episode_start = episode.history[0].get_timestamp()
     episode_end = episode.history[-1].get_timestamp()
     num_days = 1 + (episode_end - episode_start) / SECOND_PER_DAY
+    return num_days
 
-    return float(total_growth) / num_days
+def compute_avg_growth_per_day(episode, rewards):
+    total_reward = np.sum(rewards)
+    num_days = compute_episode_length_in_days(episode)
+    reward_per_day = total_reward / num_days
+    return np.exp(reward_per_day)
 
 def total_growth_relative_to_best_pick(episode, rewards):
     best_asset = None
@@ -37,21 +40,51 @@ def total_growth_relative_to_best_pick(episode, rewards):
 class MetricsLogger:
 
     def __init__(self):
-        self.episodes = []
+        self.summaries = []
+        self.current_episode = None
+        self.current_rewards = []
 
-    def new_episode(self):
-        self.episodes.append([])
+    def new_episode(self, episode):
+        # compute summary stats
+        if self.current_episode is not None:
+            stats = self._compute_summary_stats(self.current_episode, self.current_rewards)
+            self.summaries.append(stats)
+
+        # start new episode
+        self.current_episode = episode
+        self.current_rewards.clear()
 
     def log_reward(self, reward):
-        rewards = self.episodes[-1]
-        rewards.append(reward)
+        self.current_rewards.append(reward)
+
+    def finalize(self):
+        stats = self._compute_summary_stats(self.current_episode, self.current_rewards)
+        self.summaries.append(stats)
 
     def clear(self):
-        self.episodes.clear()
+        self.summaries.clear()
+        self.current_episode = None
+        self.current_rewards.clear()
+
+    @staticmethod
+    def _compute_summary_stats(episode, rewards):
+        episode_length = compute_episode_length_in_days(episode)
+
+        total_growth = compute_total_growth(rewards)
+        avg_growth_per_day = compute_avg_growth_per_day(episode, rewards)
+        relative_growth = total_growth_relative_to_best_pick(episode, rewards)
+
+        return episode_length, total_growth, avg_growth_per_day, relative_growth
 
 class EmptyLogger(MetricsLogger):
-    def new_episode(self):
+    def new_episode(self, episode):
         pass
 
     def log_reward(self, reward):
+        pass
+
+    def finalize(self):
+        pass
+
+    def clear(self):
         pass
